ef1c602d6232d7ec61c38f7ff9c7b8c4a4c97948
tz1006 <a@omg.tf>
2018-01-19 02:11:00 -0800

    Create goradar_bug.py

---
+++ goradar_bug.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*- 
+# filename: goradar_bug.py
+
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from pytz import timezone
+import sms
+import time
+
+s = requests.session()
+s.keep_alive = False
+
+def search_bug():
+    url = 'http://www.goradar.cn/portal.php?mod=list&catid=4'
+    r = s.get(url)
+    source = soup.select('dl.bbda.cl')
+    for i in source:
+        title = i.dt.text
+        if 'BUG' or 'bug' in title:
+            date = source[0].span.text.strip().split(' ')[0]
+            agency = source[0].span.text.strip().split(' ')[1]
+            today = datetime.now(timezone('Asia/Shanghai')).strftime('%y-%m-%d')
+            if date] == today:
+                text = '旅行雷达找到BUG\r%s\r%s' %（title, agency)
+                print(text)
+                sms.send_sms(16267318573, text)
+
+def start():
+    while True:
+        search_bug()
+        time.sleep(600)
+
+
+start()
